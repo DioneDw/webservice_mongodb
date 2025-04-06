5e7fff37d16b81927f12637d8b27de52e03bdc68
dione.evangelista <dione.evangelista@lecom.com.br>
2025-04-05 22:17:27 -0300

    Referenciando os posts do User

--- src/main/java/com/example/webserviceMongodb/Instantiation.java
+++ src/main/java/com/example/webserviceMongodb/Instantiation.java
@@ -27,15 +27,18 @@ public class Instantiation implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
+
         User u1 = new User(null, "Maria","maria@gmail.com");
         User u2 = new User(null, "Alex Gray","alex@gmail.com");
         User u3 = new User(null, "Bob Gray","bob@gmail.com");
         userRepository.saveAll(List.of(u1,u2,u3));
 
-        postRepository.deleteAll();
         Post post1 = new Post(null, LocalDate.parse("01/05/2025",dtf), "Partiu Viagem","Vou viajar para Porto Seguro", new AuthorRecord(u1.getId(),u1.getName()));
         Post post2 = new Post(null, LocalDate.parse("02/05/2025",dtf), "Terminal lotado","Esperando para embarcar, fuiii!!!", new AuthorRecord(u1.getId(),u1.getName()))    ;
         postRepository.saveAll(List.of(post1,post2));
 
+        u1.getPosts().addAll(List.of(post1,post2));
+        userRepository.save(u1);
     }
 }
--- src/main/java/com/example/webserviceMongodb/domain/User.java
+++ src/main/java/com/example/webserviceMongodb/domain/User.java
@@ -2,7 +2,11 @@ package com.example.webserviceMongodb.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,10 +14,18 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Setter
 @EqualsAndHashCode
 @Document
-@Builder
 public class User {
     @Id
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String name, String id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
 }
